fd66632a2f5957e4ee504253919900eea7d0952e
timnas <timna.smadja@gmail.com>
2025-03-04 13:28:09 +0200

    updated streamlit UI and experiment 1 to be regression

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import accuracy_score
 from sklearn.cluster import AgglomerativeClustering
